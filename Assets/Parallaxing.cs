fddfc8beca40652b2d48c2a941c9d9ea41271dff
Tina <TinaNguyen290@yahoo.com>
2018-11-09 23:30:30 -0800

    Finished lesson 3 on parallax backgrounds.

--- Assets/Parallaxing.cs
+++ Assets/Parallaxing.cs
@@ -19,13 +19,43 @@ public class Parallaxing : MonoBehaviour {
     private Vector3 previousCamPos;
 
 
-	// Use this for initialization
-	void Start () {
-		
+    void Awake()
+    {
+        cam = Camera.main.transform;
+    }
+
+    // Use this for initialization
+    void Start () {
+        //Previous frame had the current frame's camera position
+        previousCamPos = cam.position;
+
+        //Assigning corresponding parallaxScales
+        parallaxScales = new float[backgrounds.Length];
+        for (int i = 0; i < backgrounds.Length; i++)
+        {
+            parallaxScales[i] = backgrounds[i].position.z * -1;
+        }
+
 	}
 	
 	// Update is called once per frame
 	void Update () {
-		
+        //For each background
+        for (int i = 0; i < backgrounds.Length; i++){
+            // the parallax is the opposite of the camera movement because the previous frame multiplied by the scale
+            float parallax = (previousCamPos.x - cam.position.x) * parallaxScales[i];
+
+            // set a target x position which is the current position plus the parallax
+            float backgroundTargetPosX = backgrounds[i].position.x + parallax;
+
+            // create a target position which is the background's current position with it's target x position
+            Vector3 backgroundTargetPos = new Vector3(backgroundTargetPosX, backgrounds[i].position.y, backgrounds[i].position.z);
+
+            // fade between current position and target position using lerp
+            backgrounds[i].position = Vector3.Lerp(backgrounds[i].position, backgroundTargetPos, smoothing * Time.deltaTime);
+        }
+
+        // set the previousCamPos to the camera's position at the end of the frame
+        previousCamPos = cam.position;
 	}
 }

225e6b99c7e6c5e55551c0f9b0491ef84c7c7ee9
Tina <TinaNguyen290@yahoo.com>
2018-11-09 23:01:37 -0800

    Initial commit

---
+++ Assets/Parallaxing.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Parallaxing : MonoBehaviour {
+    // Array of all BG's and FG's to be parallaxed
+    public Transform[] backgrounds;
+
+    //Proportion of camera's movement to move the BGs by
+    private float[] parallaxScales;
+
+    //How smooth the parallax is going to be. Must be set above 0
+    public float smoothing = 1f;
+
+    //Reference to main camera's transform
+    private Transform cam;
+
+    //The position of the camera in the previous frame
+    private Vector3 previousCamPos;
+
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+}

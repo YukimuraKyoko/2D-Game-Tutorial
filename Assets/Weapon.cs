78e1fd72ec654387a8049910e0c199f078b8a593
Tina Nguyen <TinaNguyen290@yahoo.com>
2018-11-28 17:35:19 -0800

    Completed Bullet spawning and bullet trails.

--- Assets/Weapon.cs
+++ Assets/Weapon.cs
@@ -6,7 +6,12 @@ public class Weapon : MonoBehaviour {
 
     public float fireRate = 0;
     public float Damage = 10;
-    public LayerMask notToHit;
+    public LayerMask whatToHit;
+
+    public Transform BulletTrailPrefab;
+
+    float timeToSpawnEffect = 0;
+    public float effectSpawnRate = 10;
 
     float timeToFire = 0;
     Transform firepoint;
@@ -22,6 +27,7 @@ public class Weapon : MonoBehaviour {
 	
 	// Update is called once per frame
 	void Update () {
+        
 		if(fireRate == 0)
         {
             if (Input.GetButtonDown("Fire1"))
@@ -41,6 +47,27 @@ public class Weapon : MonoBehaviour {
 
     void Shoot()
     {
-        Debug.Log("Test");
+        Vector2 mousePosition = new Vector2(Camera.main.ScreenToWorldPoint(Input.mousePosition).x, Camera.main.ScreenToWorldPoint(Input.mousePosition).y);
+        Vector2 firePointPosition = new Vector2(firepoint.position.x, firepoint.position.y);
+        RaycastHit2D hit = Physics2D.Raycast(firePointPosition, mousePosition-firePointPosition, 100,whatToHit);
+        if(Time.time >= timeToSpawnEffect)
+        {
+            Effect();
+            timeToSpawnEffect = Time.time + 1 / effectSpawnRate;
+        }
+        
+
+        Debug.DrawLine(firePointPosition, (mousePosition-firePointPosition)*100, Color.cyan);
+        if(hit.collider != null)
+        {
+            Debug.Log("We hit " + hit.collider.name + " and did " + Damage + " damage.");
+            Debug.DrawLine(firePointPosition, hit.point, Color.red);
+
+        }
+    }
+
+    void Effect()
+    {
+        Instantiate(BulletTrailPrefab,firepoint.position, firepoint.rotation);
     }
 }

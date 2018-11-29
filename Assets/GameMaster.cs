0a707b5e9d6ec794ef3ccf7b6efa33a0aabda8c6
Tina Nguyen <TinaNguyen290@yahoo.com>
2018-11-28 19:15:26 -0800

    Started GM respawn for player

---
+++ Assets/GameMaster.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GameMaster : MonoBehaviour {
+
+    public static GameMaster gm;
+
+    void Start()
+    {
+        if (gm == null)
+        {
+            gm = GameObject.FindGameObjectWithTag("GM").GetComponent<GameMaster>();
+        }   
+    }
+
+    public Transform playerPrefab;
+    public Transform spawnPoint;
+    public int spawnDelay = 2;
+
+    public IEnumerator RespawnPlayer()
+    {
+        Debug.Log("Todo: add waiting for spawn sound");
+
+        //If we use yield we have to change "void" to "IEnumerator"
+        yield return new WaitForSeconds(spawnDelay);
+        Instantiate(playerPrefab, spawnPoint.position, spawnPoint.rotation);
+        Debug.Log("Todo: add spawn particles");
+    }
+
+    public static void KillPlayer(Player player)
+    {
+        Destroy(player.gameObject);
+        gm.StartCoroutine(gm.RespawnPlayer());
+    }
+}
---
+++ Assets/Player.cs
@@ -0,0 +1,43 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Player : MonoBehaviour {
+
+    //To see the class and all public variables in Unity's inspector
+    [System.Serializable]
+
+    public class PlayerStats
+    {
+        public int Health = 100;
+    }
+
+    //Creates a PlayerStats object called "playerStats"
+    public PlayerStats playerStats = new PlayerStats();
+
+    public int fallBoundary = -20;
+
+    void Update()
+    {
+        //if player's y position falls below the fall boundary
+        if(transform.position.y <= fallBoundary)
+        {
+            //Kill the player
+            DamagePlayer(99999999);
+        }
+    }
+
+    public void DamagePlayer(int damage)
+    {
+        //Damage the player
+        playerStats.Health -= damage;
+
+        //If player's HP is less than 0
+        if(playerStats.Health <= 0)
+        {
+            //Use GM's killplayer function, pass in this object.
+            GameMaster.KillPlayer(this);
+        }
+    }
+
+}
